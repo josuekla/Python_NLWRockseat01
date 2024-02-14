8f214b842e9a0bc6150cbf5e8ca7c0bc66326e44
Josué Klaysler <josueklayslerjk@gmail.com>
2024-02-14 14:14:16 -0200

    feat: Implementing Validator

--- src/errors/error_handler.py
+++ src/errors/error_handler.py
@@ -1,6 +1,23 @@
 from src.views.http_types.http_response import HttpResponse
+from src.errors.error_types.http_unprocessable_entity import HttpUnprocessableEntityError
 
 def handle_errors(error: Exception) -> HttpResponse:
+    if isinstance(error, HttpUnprocessableEntityError):
+        return HttpResponse(
+            # Envia para um log
+            #Enviar um email de notificação
+            status_code= error.status_code,
+            body={
+                "errors": [{
+                    "title": error.name,
+                    "detail": error.message
+                }]
+            }
+        )
+
+
+
+
     return HttpResponse(
         status_code= 500,
         body={
